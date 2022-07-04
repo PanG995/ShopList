018d9fba31b353fcbe2303641fcd2211dc740a54
Dominik Golik <dgolik@DGOLIK-LAP.local>
2022-07-04 19:30:27 +0200

    Add MainActivity

---
+++ app/src/main/java/com/example/shoplist/presentation/main/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.shoplist.presentation.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/shoplist/presentation/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.shoplist.presentation.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewMode() : ViewModel() {
+}
